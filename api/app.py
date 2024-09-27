e9c67e603de41b21999033a13e874d7f5e56b98d
Adam Dinan <adam1989ie@gmail.com>
2024-09-27 14:39:46 +0100

    Simplify fastapi directory structure

--- api/app.py
+++ api/app.py
@@ -1,10 +1,22 @@
 import uvicorn
-from app import app
+from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from dotenv import load_dotenv
+from app.api_routes import router
 
 load_dotenv()
 
-app = app()
+app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["http://localhost:3000"],  # frontend's domain
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+app.include_router(router)
 
 if __name__ == "__main__":
     uvicorn.run(app, host="127.0.0.1", port=5328)
---
+++ api/config.py
@@ -0,0 +1,14 @@
+# TODO: add any necessary config to this file (example below)
+
+# import os
+# from dotenv import load_dotenv
+
+# basedir = os.path.abspath(os.path.dirname(__file__))
+# load_dotenv(os.path.join(basedir, "config.env"))
+
+
+# class Config:
+#     SECRET_KEY = 'your_secret_key'
+#     ENV = os.environ.get("FLASK_ENV") or "development"
+#     SQLALCHEMY_DATABASE_URI = os.environ.get("DATABASE_URI") or "postgresql://<username>:<password>@<host>/<dbname>"
+#     SQLALCHEMY_TRACK_MODIFICATIONS = False

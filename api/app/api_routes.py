5ab773e77085cb9dac0974ed75080d8c7d9d8add
Adam Dinan <adam1989ie@gmail.com>
2024-10-04 20:22:52 +0100

    Set up backend logic for user input

--- api/app/api_routes.py
+++ api/app/api_routes.py
@@ -1,8 +1,16 @@
-from fastapi import APIRouter
+from fastapi import APIRouter, File, Form, UploadFile
+from typing import Optional
 
 router = APIRouter()
 
 
-@router.get("/api/python")
-async def hello_world():
-    return {"message": "Hello, World!"}
+@router.post("/api/enrichment")
+async def enrichment(file: Optional[UploadFile] = None, geneList: Optional[str] = Form(None)):
+
+    if file:
+        contents = await file.read()
+        gene_list = contents.decode("utf-8").splitlines()
+    else:
+        gene_list = geneList.splitlines()
+
+    return {"submitted_genes": gene_list}

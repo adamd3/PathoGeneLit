20a160f6f6d7a325f034d27b1b3eaa8365e2c829
Adam Dinan <adam1989ie@gmail.com>
2024-10-11 09:14:14 +0100

    Add database stats page

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -12,11 +12,12 @@ export default function RootLayout({
   return (
     <html lang="en">
       <head>
+        
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" />
-        <link href="https://fonts.googleapis.com/css2?family=Fira+Code:wght@300..700&family=Merriweather:ital,wght@0,300;0,400;0,700;0,900;1,300;1,400;1,700;1,900&family=Outfit:wght@100..900&family=Source+Code+Pro:ital,wght@0,200..900;1,200..900&display=swap" rel="stylesheet" />
-       </head>
-      <body className="container max-w-screen-2xl mx-auto p-8 ">
+        <link href="https://fonts.googleapis.com/css2?family=Plus+Jakarta+Sans:ital,wght@0,200..800;1,200..800&display=swap" rel="stylesheet" />
+      </head>
+      <body className="container max-w-screen-xl mx-auto p-8 bg-dark-grey">
         <Header />  
         
         <main>
---
+++ src/app/stats.tsx
@@ -0,0 +1,31 @@
+'use client'
+import { useStatsQuery } from '@/generated/graphql'
+import classNames from 'classnames'
+
+export default function Stats({
+  bold,
+  show_sets_analyzed,
+  show_gene_sets,
+  show_pmcs,
+  show_publications,
+}: {
+  bold?: boolean,
+  show_sets_analyzed?: boolean,
+  show_gene_sets?: boolean,
+  show_pmcs?: boolean,
+  show_publications?: boolean
+}) {
+  const { data } = useStatsQuery({ pollInterval: 60000 })
+
+  if (show_gene_sets) {
+    return (data?.geneSets?.totalCount !== undefined && show_gene_sets) ? <><span className={classNames({'font-bold': bold})}>{Intl.NumberFormat("en-US", {}).format(data.geneSets.totalCount)}</span>&nbsp;gene sets</> : <span className='loading'>loading</span>
+  } else if (show_pmcs){
+    return (data?.pmcs?.totalCount !== undefined ? <><span className={classNames({'font-bold': bold})}>{Intl.NumberFormat("en-US", {}).format(data.pmcs.totalCount)}</span>&nbsp;articles</> : <span className='loading'>loading</span>)
+  } else if (show_publications) {
+    return (data?.pmcStats?.nPublicationsProcessed !== undefined ? <><span className={classNames({'font-bold': bold})}>{Intl.NumberFormat("en-US", {}).format(data.pmcStats.nPublicationsProcessed)}</span>&nbsp;PMC articles</> : <span className='loading'>loading</span>) 
+  } else if (show_sets_analyzed) {
+    return (data?.userGeneSets?.totalCount !== undefined ? <><span className={classNames({'font-bold': bold})}>{Intl.NumberFormat("en-US", {}).format(data.userGeneSets.totalCount)}</span>&nbsp;sets analyzed</> : <span className='loading'>loading</span>)
+  } else{
+    return null
+  }
+}

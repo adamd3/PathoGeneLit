cdb975f00190bb476d95f0aba9244658ccc50297
Adam Dinan <adam1989ie@gmail.com>
2024-10-13 19:47:14 +0100

    Restyle page

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,7 +3,6 @@ import Header from '../components/Header';
 import Footer from '../components/Footer';
 import MyApolloProvider from '@/lib/apollo/provider';
 
-
 export default function RootLayout({
   children,
 }: {
@@ -12,21 +11,22 @@ export default function RootLayout({
   return (
     <html lang="en">
       <head>
-        
         <link rel="preconnect" href="https://fonts.googleapis.com" />
-        <link rel="preconnect" href="https://fonts.gstatic.com" />
-        <link href="https://fonts.googleapis.com/css2?family=Plus+Jakarta+Sans:ital,wght@0,200..800;1,200..800&display=swap" rel="stylesheet" />
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
+        <link href="https://fonts.googleapis.com/css2?family=Plus+Jakarta+Sans:ital,wght@0,200..800;1,200..800&family=Inter:wght@400;500;600;700&display=swap" rel="stylesheet" />
       </head>
-      <body className="container max-w-screen-xl mx-auto p-8 bg-dark-grey">
-        <Header />  
+      <body className="min-h-screen flex flex-col bg-gray-100 text-gray-900">
+        <Header />
         
-        <main>
+        <main className="flex-grow container max-w-screen-xl mx-auto p-8">
           <MyApolloProvider>
-          {children}
+            <div className="bg-white shadow-md rounded-lg p-6">
+              {children}
+            </div>
           </MyApolloProvider>
         </main>
         
-        <Footer />  
+        <Footer />
       </body>
     </html>
   );
